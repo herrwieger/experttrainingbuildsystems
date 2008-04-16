70d07cd9a2ce55e064a25b923a469cf0205a853d
thomas.wieger@gmx.de <thomas.wieger@gmx.de@77961049-1948-0410-b0a4-f1485b09de59>
2008-04-16 14:55:32 +0000

    maven in seu eingechecked für "Quickstart". Projektstruktur unter trunk auf hierarchisch umgestellt. Template-Dateien für lokale SEU eingechecked.

---
+++ trunk/environment_template.bat
@@ -0,0 +1,15 @@
+@echo off
+REM -----------------------------------------------
+REM Customization necessary
+REM -----------------------------------------------
+
+set JAVA_HOME=C:\Apps\Java\jdk1.6.0
+
+
+
+REM -----------------------------------------------
+REM Derived properties. NO customization necessary
+REM -----------------------------------------------
+
+set PATH=%PATH%;%~dp0\seu\maven\maven-2.0.9\bin;%JAVA_HOME\bin
+set MAVEN_OPTS="-Xmx512m"
