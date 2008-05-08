f736f6c6d1ef4dbdb94f28f8e5ce12262964a29e
jerger <jerger@jerger.org>
2008-05-08 13:53:33 +0000

    Präsentation gem Feedback überarbeitet

---
+++ praesentationen/MavenEclipse.mm
@@ -0,0 +1,49 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1209546615119" ID="Freemind_Link_1821415798" MODIFIED="1209546725112" TEXT="IDEIntegration">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1209547148928" ID="Freemind_Link_1291250273" LINK="http://docs.codehaus.org/display/M2ECLIPSE/Home" MODIFIED="1209549157985" POSITION="left" TEXT="M2Eclipse">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1209546682812" ID="_" MODIFIED="1209547159479" TEXT="Features">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1209546698524" ID="Freemind_Link_1285592526" MODIFIED="1209547193347" TEXT="integriertes resource filtering">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1209547133225" ID="Freemind_Link_410236078" LINK="http://docs.codehaus.org/display/M2ECLIPSE/Separate+Eclipse+and+Maven+output+folders" MODIFIED="1209548920619" TEXT="getrennte target-eclipse verz.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1209547510911" ID="Freemind_Link_346394829" MODIFIED="1209547717345" TEXT="Create- / Import-Wizards">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1209547717982" ID="Freemind_Link_1107286048" MODIFIED="1209547730775" TEXT="Autorefresh f&#xfc;r den Classpath Container">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1209549063270" ID="Freemind_Link_50074239" LINK="http://code.google.com/p/q4e/" MODIFIED="1209549103358" POSITION="right" TEXT="q4eclipse">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1209549076685" ID="Freemind_Link_1747575199" MODIFIED="1209549080280" TEXT="features">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1209549295396" ID="Freemind_Link_1083587131" MODIFIED="1209549313517" TEXT="running Maven goals from the IDE">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1209549285549" ID="Freemind_Link_727722767" MODIFIED="1209549350054" TEXT="dependency managing using the Maven POM, with automatic download of dependencies">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1209549209714" ID="Freemind_Link_920613849" MODIFIED="1209549364108" TEXT=" keeping Eclipse classpath synchronized with Maven POM">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1209549186012" ID="Freemind_Link_394178530" MODIFIED="1209549382174" TEXT="dependency graphing direct import of Maven 2 projects">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1209549384256" ID="Freemind_Link_1990580625" MODIFIED="1209549394836" TEXT="wizards for import &amp; new">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1209549503667" ID="Freemind_Link_699615117" LINK="http://maven.apache.org/guides/mini/guide-ide-eclipse.html" MODIFIED="1209549540448" POSITION="right" TEXT="eclipse:eclipse">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
